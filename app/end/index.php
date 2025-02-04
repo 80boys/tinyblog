bb2b354c93ff90bd72b33d2c13dd6756762fdf9b
wheng-win <admin@80boys.com>
2025-02-04 08:59:13 +0800

    调整后台展示

--- app/end/index.php
+++ app/end/index.php
@@ -12,40 +12,29 @@ include(PROJECT_ROOT . "/app/block/navi.php");
     <article>
         <h2>博客管理</h2>
         <section>
-            <!-- 博客列表 -->
-            <table>
-                <thead>
-                    <tr>
-                        <th>标题</th>
-                        <th>描述</th>
-                        <th>时间</th>
-                        <th>操作</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $dt = new \App\Utils\DirectoryTraverser();
-                    $result = $dt->getAllBlogs();
-                    $blogs = $result['blogs'];
-                    $totalPages = $result['totalPages'];
-                    $currentPage = $result['currentPage'];
-                    // 遍历博客列表并输出
-                    foreach ($blogs as $blog) {
-                        echo '<tr>';
-                        echo '<td width="200px"><a href="' . BASE_PATH . '/app/blogs/' . rtrim($blog['path'], '.json') . '.html">' . $blog['title'] . '</a></td>';
-                        echo '<td>' . $blog['subtitle'] . '</td>';
-                        echo '<td width="100px" style="text-align: center;" >' . $blog['date'] . '</td>';
-                        echo '<td width="80px"><a href="' . BASE_PATH . '/app/end/edit.html?blog_path=' . $blog['path'] . '">编辑</a> | <a href="' . BASE_PATH . '/app/end/del.html?blog_path=' . $blog['path'] . '">删除</a></td>';
-                        echo '</tr>';
-                    }
-                    ?>
-                </tbody>
-            </table>
-
-            <!-- 分页 -->
-            <?php include(PROJECT_ROOT . "/app/block/pagination.php"); ?>
-            
+            <?php
+                $dt = new \App\Utils\DirectoryTraverser();
+                $result = $dt->getAllBlogs();
+                $blogs = $result['blogs'];
+                $totalPages = $result['totalPages'];
+                $currentPage = $result['currentPage'];
+                // 遍历博客列表并输出
+                foreach ($blogs as $blog) {
+                    echo '<section class="blog-item">';
+                    echo '<p> 博客标题: <a href="' . BASE_PATH . '/app/blogs/' . rtrim($blog['path'], '.json') . '.html">' . $blog['title'] . '</a></p>';
+                    echo '<p> 博客描述: ' . $blog['subtitle'] . '</p>';
+                    echo '<p> 编辑时间: ' . $blog['date'] . '</p>';
+                    echo '<div class="actions">';
+                    echo '  <a href="' . BASE_PATH . '/app/end/edit.html?blog_path=' . $blog['path'] . '">编辑</a> | <a href="' . BASE_PATH . '/app/end/del.html?blog_path=' . $blog['path'] . '">删除</a>';
+                    echo '</div>';
+                    echo '</section>';
+                    echo '<hr>';
+                }
+            ?>
         </section>
+
+        <!-- 分页 -->
+        <?php include(PROJECT_ROOT . "/app/block/pagination.php"); ?>
     </article>
 </main>
 
