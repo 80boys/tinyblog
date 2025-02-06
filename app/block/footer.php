164d3d4478f1357374fda2956766d3a8342f3bdd
wuheng <admin@80boys.com>
2025-02-06 10:20:24 +0800

    完善内容

--- app/block/footer.php
+++ app/block/footer.php
@@ -1,8 +1,9 @@
 <footer>
     <?php
-        $settings = getBlogsConfig();
+    $settings = getBlogsConfig();
     ?>
-    <p>© <?php echo date("Y"); ?> <?php echo $settings['website_name'] ?> <a target="_blank" rel="noopener noreferrer" href="https://beian.miit.gov.cn"> <?php echo $settings['beian_number'] ?></a></p>
+    <p>© <?php echo date("Y"); ?> <?php echo $settings['website_name']; ?> <a target="_blank" rel="noopener noreferrer" href="https://beian.miit.gov.cn"> <?php echo $settings['beian_number']; ?></a></p>
 </footer>
 </body>
+
 </html>
