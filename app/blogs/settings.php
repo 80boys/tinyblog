a217fbf63249476e78066e5f1e8625678803f06a
80boys <80boys@example.com>
2025-03-20 20:48:29 +0800

    update

---
+++ app/blogs/settings.php
@@ -0,0 +1,11 @@
+<?php return array (
+  'website_name' => '枫桥驿站',
+  'beian_number' => '黑ICP备16002822号-5',
+  'contact_email' => 'admin@80boys.com',
+  'wechat_id' => 'love2098',
+  'qiniu_access_key' => '79OD2IOqm6PCJsrhbfzWlJUP2ol3m6TZCtdcAR7X',
+  'qiniu_secret_key' => 'Yyr3kyBUk88mbG5CgVv2P6Pyz5Xwi9Vl3GjciUQL',
+  'qiniu_bucket' => 'file-static',
+  'qiniu_domain' => 'cdn.maplebridge.net',
+  'qiniu_accelerate_domain' => 'https://blog.maplebridge.net',
+);
